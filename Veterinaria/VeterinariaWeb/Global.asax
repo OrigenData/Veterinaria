f9e26625f338c9a8e5eb673ebd37bb9f3dfe806c
Cristian McGrath <mcgrath.es@openmailbox.org>
2020-05-30 04:04:22 -0700

    init repo

---
+++ Veterinaria/VeterinariaWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VeterinariaWeb.MvcApplication" Language="C#" %>
---
+++ Veterinaria/VeterinariaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VeterinariaWeb.Startup))]
+namespace VeterinariaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
